3444be41225134562a1b43bba95441d12cc1d901
Delbin George <imdelbingeorge@gmail.com>
2024-08-03 19:45:12 +0530

    Added some changes in simmer

--- brevity-client/components/Skeleton/FeedContentLoader.js
+++ brevity-client/components/Skeleton/FeedContentLoader.js
@@ -31,7 +31,33 @@ const FeedContentLoader = () => {
           />
         </View>
       </View>
-      <View style={{paddingHorizontal: 15, marginTop: 5}}>
+      <View style={{paddingHorizontal: 15, marginTop: 5, marginBottom: 15}}>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            columnGap: 10,
+            marginBottom: 10,
+          }}>
+          <Skeleton circle width={25} color={'rgba(0,0,0,0.1)'} height={25} />
+          <Skeleton
+            count={1}
+            width={'50%'}
+            color={'rgba(0,0,0,0.1)'}
+            height={13}
+          />
+        </View>
+        <View>
+          <Skeleton
+            count={7}
+            width={'100%'}
+            height={14}
+            color={'rgba(0,0,0,0.1)'}
+            borderRadius={4}
+          />
+        </View>
+      </View>
+      <View style={{paddingHorizontal: 15, marginTop: 5, marginBottom: 15}}>
         <View
           style={{
             flexDirection: 'row',
